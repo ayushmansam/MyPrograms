b1027ebcf746a696d86bb09c5b0e9fa00c6ae8ce
ayushmansam <86476157+ayushmansam@users.noreply.github.com>
2022-11-02 18:18:22 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,13 @@
+def program3():
+    print("Program3")
+    for i in range(100, 0, -2):
+        print("Num is: ", i)
+
+def program4():
+    print("program4")
+    i = 1
+    while i<=100:
+        print(i)
+        i+=1
+
+program4()
